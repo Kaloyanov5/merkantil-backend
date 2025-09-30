583ce9bb8c17654702a8a8df3b1f7b98b81da99e
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-09-30 19:53:04 +0300

    Initial maven files commit

---
+++ src/main/java/github/kaloyanov5/merkantil/MerkantilApplication.java
@@ -0,0 +1,13 @@
+package github.kaloyanov5.merkantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerkantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerkantilApplication.class, args);
+	}
+
+}
