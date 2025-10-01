d604f3e9d8e26cd5296f7742b0e473127870132a
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-10-01 19:25:36 +0300

    Created `Portfolio` entity for each user's stock holdings (todo: currentValue)

---
+++ src/main/java/github/kaloyanov5/merkantil/entity/Portfolio.java
@@ -0,0 +1,36 @@
+package github.kaloyanov5.merkantil.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Portfolio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonBackReference
+    private User user;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String symbol;
+
+    @NotNull
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @NotNull
+    @Column(nullable = false)
+    private Double averageBuyPrice;
+
+    // CURRENT VALUE
+}
