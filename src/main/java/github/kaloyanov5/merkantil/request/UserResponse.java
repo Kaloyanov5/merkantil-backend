e0fcb4354604039068ace8533d63ec5237f16aa7
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-10-04 19:28:43 +0300

    Added DTOs

---
+++ src/main/java/github/kaloyanov5/merkantil/request/UserResponse.java
@@ -0,0 +1,18 @@
+package github.kaloyanov5.merkantil.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String username;
+    private String email;
+    private Double balance;
+    private LocalDateTime createdAt;
+}
