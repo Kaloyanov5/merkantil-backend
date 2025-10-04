1d38a658f102d057a1e086fe146a9d66b3cce872
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-10-04 19:55:21 +0300

    `AuthResponse` DTO

---
+++ src/main/java/github/kaloyanov5/merkantil/request/AuthResponse.java
@@ -0,0 +1,11 @@
+package github.kaloyanov5.merkantil.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String message;;
+    private UserResponse user;
+}
