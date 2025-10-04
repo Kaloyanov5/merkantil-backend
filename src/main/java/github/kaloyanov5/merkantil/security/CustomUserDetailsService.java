daa10b6532ac4d32ebb60231a28dca7e653c7017
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-10-04 18:58:51 +0300

    Implemented custom `UserDetails` and `UserDetailsService`

---
+++ src/main/java/github/kaloyanov5/merkantil/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package github.kaloyanov5.merkantil.security;
+
+import github.kaloyanov5.merkantil.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
