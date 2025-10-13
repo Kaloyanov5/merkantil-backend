db79ec52f5e6fb94ebb7461cfb870ce3529e9bda
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-10-13 22:37:58 +0300

    Enabled caching in the main class

--- src/main/java/github/kaloyanov5/merkantil/MerkantilApplication.java
+++ src/main/java/github/kaloyanov5/merkantil/MerkantilApplication.java
@@ -2,12 +2,14 @@ package github.kaloyanov5.merkantil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
+@EnableCaching
 public class MerkantilApplication {
 
 	public static void main(String[] args) {
